b57ab164077b1c4052d523a08e05d7bc7d3ba460
nasrulirfan <nasrulirfan02@gmail.com>
2024-06-08 17:08:25 +0800

    Added migrations

---
+++ laravel10-backend/database/migrations/2024_06_08_090401_create_product_master_list_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_master_list', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('brand');
+            $table->string('model');
+            $table->string('capacity');
+            $table->unsignedInteger('quantity');  // Unsigned as quantity should not be negative
+            $table->timestamps(); 
+
+                // Adding indexes for frequently searched columns
+            $table->index('type');
+            $table->index('brand');
+            $table->index('model');
+            $table->index('capacity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_master_list');
+    }
+};
