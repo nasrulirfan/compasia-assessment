47783c1379a03edc3d0ffc21437bd610b30e0593
nasrulirfan <nasrulirfan02@gmail.com>
2024-06-09 16:17:07 +0800

    Cleanup

--- laravel10-backend/routes/api.php
+++ laravel10-backend/routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/product', ProductMasterListController::class);
+Route::resource('/product', ProductMasterListController::class)->except(['create', 'edit']);;
 Route::post('/product/upload', [ProductMasterListController::class, 'uploadFile']);
